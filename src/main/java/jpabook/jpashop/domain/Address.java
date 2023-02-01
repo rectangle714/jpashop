8526953d03896db3ff35a84725e2fb259af23173
PHG <rectangle714@naver.com>
2023-02-01 21:25:36 +0900

    20230201 저녁 공부 커밋

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -3,7 +3,6 @@ package jpabook.jpashop.domain;
 import lombok.Data;
 
 import javax.persistence.Embeddable;
-import javax.persistence.Entity;
 
 @Embeddable
 @Data
@@ -13,4 +12,9 @@ public class Address {
     private String street;
     private String zipcode;
 
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
--- src/main/java/jpabook/jpashop/domain/item/Movie.java
+++ src/main/java/jpabook/jpashop/domain/item/Movie.java
@@ -10,4 +10,7 @@ import javax.persistence.Entity;
 @Data
 public class Movie extends Item {
 
+    private String director;
+    private String actor;
+
 }
