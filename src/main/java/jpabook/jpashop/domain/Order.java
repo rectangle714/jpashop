734de7890c2ed566ba8e05c493248f9e04bbc4c3
PHG <rectangle714@naver.com>
2023-02-05 17:51:55 +0900

    20230205 저녁 공부 커밋

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -33,7 +33,7 @@ public class Order {
     private OrderStatus status; //주문상태 ORDER, CANCEL
 
     // 연관관계 메서드
-    public void SetMember(Member member) {
+    public void setMember(Member member) {
         this.member = member;
         member.getOrders().add(this);
     }
@@ -43,8 +43,39 @@ public class Order {
         orderItem.setOrder(this);
     }
 
-    public void SetDelivery(Delivery delivery) {
+    public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+
+    // 생성 메서드
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+
+        for(OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+
+        return order;
+    }
+
+    // 비즈니스 로직 //
+    /*
+     주문 취소
+    */
+    public void cancelOrder(Order order) {
+        if (delivery.getStaus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : this.orderItems) {
+
+        }
+    }
+
 }
--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -1,12 +1,17 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.security.RunAs;
+import javax.persistence.EntityManager;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,23 +20,47 @@ import static org.junit.jupiter.api.Assertions.*;
 @Transactional
 public class MemberServiceTest {
 
+    @Autowired
+    private MemberService memberService;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+//    @Autowired
+//    private EntityManager em;
+
     @Test
+    //@Rollback(value = false)
     public void 회원가입() throws Exception {
         //given
+        Member member = new Member();
+        member.setName("1111");
 
         //when
+        Long saveId = memberService.join(member);
 
         //then
+        assertEquals(member, memberRepository.findOne(saveId));
 
     }
 
     @Test
     public void 중복_회원_예외() throws Exception {
         //given
+        Member mem1 = new Member();
+        mem1.setName("2번쨰");
+
+        Member mem2 = new Member();
+        mem2.setName("2번쨰");
 
         //when
+        memberService.join(mem1);
+        assertThrows(IllegalStateException.class, () -> {
+            memberService.join(mem2);
+        });
 
         //then
+//        fail("예외가 발생해야한다.");
 
     }
 }
